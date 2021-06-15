69742c629519c0b3c8280fa90ec4a45c8d876c38
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-15 17:40:19 -0300

    Adicionei três exercícios extras resolvidos

--- extra-10/index.js
+++ extra-10/index.js
@@ -1,1 +1,11 @@
-const caractere = "E";
+const caractere = "10";
+
+if(caractere === "A" || caractere === "E" || caractere === "I" || caractere === "O" || caractere === "U"){
+    console.log("Vogal maiúscula");
+} else if(caractere === "a" || caractere === "e" || caractere === "i" || caractere === "o" || caractere === "u"){
+    console.log("Vogal minúscula");
+} else if(caractere > 0){
+    console.log("Número");
+} else{
+    console.log("Consoante");
+}
--- extra-11/index.js
+++ extra-11/index.js
@@ -6,3 +6,8 @@ const mesesDecorridos = 12;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores (em centavos). Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 1000000;
+
+if(rendaMensalEmCentavos > 200000){
+    let parcela = (0.18 * rendaMensalEmCentavos) / 100;
+    console.log(`O valor da parcela desse mês é de ${parcela} reais`);
+}
