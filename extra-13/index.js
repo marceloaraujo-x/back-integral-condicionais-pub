e3b689c6b811deb2d8f4e91328fba129c35e7d98
Marcelo Araújo <marcelo.niklaus@gmail.com>
2021-06-15 22:24:22 -0300

    adicionei os exercícios extras restantes

--- extra-13/index.js
+++ extra-13/index.js
@@ -4,3 +4,13 @@ const tipoDePagamento = "credito";
 //valor da mercadoria (centavos)
 const valorDoProduto = 13000;
 
+if(tipoDePagamento === "debito" || tipoDePagamento === "dinheiro"){
+    let valor = (valorDoProduto - valorDoProduto * 0.1) / 100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`);
+} else if(tipoDePagamento === "cheque"){
+    let valor = (valorDoProduto - valorDoProduto * 0.03) / 100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`);
+} else if(tipoDePagamento === "credito"){
+    let valor = (valorDoProduto - valorDoProduto * 0.05) / 100;
+    console.log(`Valor a ser pago: R$${valor.toFixed(2)}`);
+}
--- extra-17/index.js
+++ extra-17/index.js
@@ -6,3 +6,9 @@ const quantidadeDoParcelamento = 10;
 
 //valor pago
 const valorPago = 300;
+
+if(valorPago != valorDoProduto){
+    let numParcela = quantidadeDoParcelamento - valorPago / (valorDoProduto / quantidadeDoParcelamento);
+    let parcelas = valorDoProduto / quantidadeDoParcelamento;
+    console.log(`Restam ${numParcela} de R$ ${(parcelas / 100).toFixed(2)}`);
+}
